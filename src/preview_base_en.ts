0a83590936231de5feecc8b0880033d3ba9b99e7
Tannin <sherb@gmx.net>
2016-06-04 19:47:34 +0200

    translation

---
+++ src/preview_base_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>PreviewBase</name>
+    <message>
+        <location filename="previewbase.cpp" line="66"/>
+        <source>Supports previewing various types of data files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
