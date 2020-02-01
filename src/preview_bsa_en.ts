fa6bf5f64f92915194e737b70562e4520468f463
AL <26797547+Al12rs@users.noreply.github.com>
2020-02-01 23:38:10 +0100

    Update trnaslations

--- src/preview_bsa_en.ts
+++ src/preview_bsa_en.ts
@@ -4,13 +4,36 @@
 <context>
     <name>PreviewBsa</name>
     <message>
-        <location filename="previewbsa.cpp" line="72"/>
+        <location filename="previewbsa.cpp" line="80"/>
         <source>Supports previewing contents of Bethesda Archive files, BSAs and BA2s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="previewbsa.cpp" line="88"/>
+        <location filename="previewbsa.cpp" line="96"/>
         <source>Enable previewing of BSA file contents</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="previewbsa.cpp" line="134"/>
+        <source>Archive Format: %1 , Compression: %2 , File count: %3 , Version: %4 , Archive type: %5 , Archive flags: %6 , Contents flags: %7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="previewbsa.cpp" line="137"/>
+        <source>yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="previewbsa.cpp" line="137"/>
+        <source>no</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimpleFileTreeModel</name>
+    <message>
+        <location filename="simplefiletreemodel.cpp" line="22"/>
+        <source>File Name</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
